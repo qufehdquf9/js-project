c272d919ad9f44029fb2deef65e4a5565aa184e1
choi youngjin <qufehdquf9@naver.com>
2018-05-04 14:16:22 +0900

    modify

--- map/map.js
+++ map/map.js
@@ -1,87 +1,91 @@
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
 var map;
 var infoWindow;
 var service;
 var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 35.140, lng: 129.100},
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 35.140, lng: 129.100},
             zoom: 14,
-            styles: [{
-            stylers: [{ visibility: 'simplified' }]
-        }, {
-            elementType: 'labels',
-            stylers: [{ visibility: 'off' }]
-        }]
-        });
+    styles: [{
+      stylers: [{ visibility: 'simplified' }]
+    }, {
+      elementType: 'labels',
+      stylers: [{ visibility: 'off' }]
+    }]
+  });
+  var infoWindow1 = new google.maps.InfoWindow({map: map});
 
-        var infoWindow = new google.maps.InfoWindow({map: map});
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+      infoWindow1.setPosition(pos);
+      infoWindow1.setContent('나의 위치');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow1, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow1, map.getCenter());
+  }
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('나의 위치');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
+function handleLocationError(browserHasGeolocation, infoWindow1, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
+  infoWindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
+
+  // The idle event is a debounced event, so we can query & listen without
+  // throwing too many requests at the server.
+  map.addListener('idle', performSearch);
+}
+
+function performSearch() {
+  var request = {
+    bounds: map.getBounds(),
+    keyword: 'Obstetrics'
+  };
+  service.radarSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status !== google.maps.places.PlacesServiceStatus.OK) {
+    console.error(status);
+    return;
+  }
+  for (var i = 0, result; result = results[i]; i++) {
+    addMarker(result);
+  }
+}
+
+function addMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    icon: iconBase + 'hospitals_maps.png'
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    service.getDetails(place, function(result, status) {
+      if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        console.error(status);
+        return;
       }
-        service = new google.maps.places.PlacesService(map);
-
-        // The idle event is a debounced event, so we can query & listen without
-        // throwing too many requests at the server.
-        map.addListener('idle', performSearch);
-      }
-
-      function performSearch() {
-        var request = {
-          bounds: map.getBounds(),
-          keyword: 'Obstetrics'
-        };
-        service.radarSearch(request, callback);
-      }
-
-      function callback(results, status) {
-        if (status !== google.maps.places.PlacesServiceStatus.OK) {
-          console.error(status);
-          return;
-        }
-        for (var i = 0, result; result = results[i]; i++) {
-          addMarker(result);
-        }
-      }
-
-      function addMarker(place) {
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location,
-          icon: iconBase + 'hospitals_maps.png'
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          service.getDetails(place, function(result, status) {
-            if (status !== google.maps.places.PlacesServiceStatus.OK) {
-              console.error(status);
-              return;
-            }
-            infoWindow.setContent(result.name);
-            infoWindow.open(map, marker);
-          });
-        });
-      }
+      infoWindow.setContent(result.name);
+      infoWindow.open(map, marker);
+    });
+  });
+}
